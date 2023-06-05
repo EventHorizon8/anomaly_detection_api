7de4c2b61487658f8e498f37edaf9d252de4efbc
EventHorizon8 <t.babarina@gmail.com>
2023-06-05 16:53:13 +0200

    gitignore files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
-from flask import Flask
+import datetime
+
+from flask import Flask, request
 
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_pyfile('settings.py')
 
-    @app.route('/')
-    def index():
-        return {'hello': 'world'}
+    @app.post('/logs')
+    def logs_add_post():
+        data = request.json
+        dataLogs = data.get('logs')
+        for log in dataLogs:
+            log['anomaly_score'] = 0.85
+            log['result_timestamp'] = datetime.datetime.now()
+        return dataLogs
 
     return app
