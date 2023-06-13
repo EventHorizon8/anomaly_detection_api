b536af1fe52d033da526c9e726aace8bfe5c6a08
EventHorizon8 <t.babarina@gmail.com>
2023-06-13 12:10:32 +0300

    remove json

--- app/__init__.py
+++ app/__init__.py
@@ -22,70 +22,7 @@ def create_app():
     def logs_add_post():
         start = datetime.datetime.now()
         data = request.json
-        data_logs = pd.json_normalize([
-          {
-            "timestamp": 129.050634,
-            "processId": 382,
-            "threadId": 382,
-            "parentProcessId": 1,
-            "userId": 101,
-            "mountNamespace": 4026532232,
-            "processName": "systemd-resolve",
-            "hostName": "ip-10-100-1-217",
-            "eventId": 41,
-            "eventName": "socket",
-            "stackAddresses": [
-              140159195621643,
-              140159192455417,
-              94656731598592
-            ],
-            "argsNum": 3,
-            "returnValue": 15,
-            "args": "[{'name': 'domain', 'type': 'int', 'value': 'AF_UNIX'}, {'name': 'type', 'type': 'int', 'value': 'SOCK_DGRAM|SOCK_CLOEXEC'}, {'name': 'protocol', 'type': 'int', 'value': 0}]",
-
-          },
-          {
-            "timestamp": 129.051238,
-            "processId": 379,
-            "threadId": 379,
-            "parentProcessId": 1,
-            "userId": 100,
-            "mountNamespace": 4026532231,
-            "processName": "systemd-network",
-            "hostName": "ip-10-100-1-217",
-            "eventId": 41,
-            "eventName": "socket",
-            "stackAddresses": [
-              139853228042507,
-              93935071185801,
-              93935080775184
-            ],
-            "argsNum": 3,
-            "returnValue": 15,
-            "args": "[{'name': 'domain', 'type': 'int', 'value': 'AF_UNIX'}, {'name': 'type', 'type': 'int', 'value': 'SOCK_DGRAM|SOCK_CLOEXEC'}, {'name': 'protocol', 'type': 'int', 'value': 0}]",
-
-          },
-          {
-            "timestamp": 129.051434,
-            "processId": 1,
-            "threadId": 1,
-            "parentProcessId": 0,
-            "userId": 0,
-            "mountNamespace": 4026531840,
-            "processName": "systemd",
-            "hostName": "ip-10-100-1-217",
-            "eventId": 1005,
-            "eventName": "security_file_open",
-            "stackAddresses": [
-              140362867191588,
-              8103505641674584000
-            ],
-            "argsNum": 4,
-            "returnValue": 0,
-            "args": "[{'name': 'pathname', 'type': 'const char*', 'value': '/proc/382/cgroup'}, {'name': 'flags', 'type': 'int', 'value': 'O_RDONLY|O_LARGEFILE'}, {'name': 'dev', 'type': 'dev_t', 'value': 5}, {'name': 'inode', 'type': 'unsigned long', 'value': 38584}]",
-
-          }
-        ])
+        data_logs = pd.json_normalize(data.get('logs'))
 
         predict_dataset = prepare_tensor_data(prepare_data(data_logs))
         # Load the models
